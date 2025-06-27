93fe4c5b90ef45ee2fa65e2f6a661a1c13dc38ba
YinChihHsiang <Chihhsiangyin2001@gmail.com>
2025-06-27 11:26:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,52 +11,46 @@ from bs4 import BeautifulSoup as bs
 import urllib.request
 
 from github_issue import make_github_issue
-from config import NEW_SUB_URL, KEYWORD_LIST, KEYWORD_EX_LIST, USERNAME
+from config import NEW_SUB_URLS, KEYWORD_LIST, KEYWORD_EX_LIST, USERNAME
 
 def main(args):
-
-    
-    # TOKEN = args.token
-    
-    page = urllib.request.urlopen(NEW_SUB_URL)
-    soup = bs(page, 'html.parser')
-    content = soup.body.find("div", {'id': 'content'})
-
-    issue_title = content.find("h3").text.split("(")[0].strip()
-    dt_list = content.dl.find_all("dt")
-    dd_list = content.dl.find_all("dd")
-    arxiv_base = "https://arxiv.org/abs/"
-
-    #print(dt_list)
-    assert len(dt_list) == len(dd_list)
-
     keyword_list = KEYWORD_LIST
     keyword_ex_list = KEYWORD_EX_LIST
     #keyword_dict = {key: [] for key in keyword_list}
     keyword_dict = []
+    
+    # TOKEN = args.token
+    for url in NEW_SUB_URLS:
+        page = urllib.request.urlopen(url)
+        soup = bs(page, 'html.parser')
+        content = soup.body.find("div", {'id': 'content'})
 
-    for i in range(len(dt_list)):
-        paper = {}
-        paper_number = dt_list[i].text.strip().split(":")[1][:11] #[2].split(":")[-1]
-        paper['main_page'] = arxiv_base + paper_number
-        paper['pdf'] = arxiv_base.replace('abs', 'pdf') + paper_number
-        #print(dt_list[i])
-        #print(dd_list[i])
+        issue_title = content.find("h3").text.split("(")[0].strip()
+        dt_list = content.dl.find_all("dt")
+        dd_list = content.dl.find_all("dd")
+        arxiv_base = "https://arxiv.org/abs/"
 
-        paper['title'] = dd_list[i].find("div", {"class": "list-title mathjax"}).text.replace("Title:\n", "").strip()
-        paper['authors'] = dd_list[i].find("div", {"class": "list-authors"}).text.replace("Authors:\n", "").replace("\n", "").strip()
-        paper['subjects'] = dd_list[i].find("div", {"class": "list-subjects"}).text.replace("Subjects: ", "").strip()
-        paper['abstract'] = dd_list[i].find("p", {"class": "mathjax"}).text.replace("\n", " ").strip()
-                        
-        inclu=0
-        for keyword in keyword_list:
-            if keyword.lower() in paper['abstract'].lower() or keyword.lower() in paper['title'].lower():
-                inclu=1
-        for keyword_ex in keyword_ex_list:
-            if (keyword_ex.lower() in paper['abstract'].lower())==1:
-                inclu=0
-        if inclu==1: 
-            keyword_dict.append(paper)
+        assert len(dt_list) == len(dd_list)
+
+        for i in range(len(dt_list)):
+            paper = {}
+            paper_number = dt_list[i].text.strip().split(":")[1][:11]
+            paper['main_page'] = arxiv_base + paper_number
+            paper['pdf'] = arxiv_base.replace('abs', 'pdf') + paper_number
+            paper['title'] = dd_list[i].find("div", {"class": "list-title mathjax"}).text.replace("Title:\n", "").strip()
+            paper['authors'] = dd_list[i].find("div", {"class": "list-authors"}).text.replace("Authors:\n", "").replace("\n", "").strip()
+            paper['subjects'] = dd_list[i].find("div", {"class": "list-subjects"}).text.replace("Subjects: ", "").strip()
+            paper['abstract'] = dd_list[i].find("p", {"class": "mathjax"}).text.replace("\n", " ").strip()
+
+            inclu = 0
+            for keyword in keyword_list:
+                if keyword.lower() in paper['abstract'].lower() or keyword.lower() in paper['title'].lower():
+                    inclu = 1
+            for keyword_ex in keyword_ex_list:
+                if (keyword_ex.lower() in paper['abstract'].lower()) == 1:
+                    inclu = 0
+            if inclu == 1:
+                keyword_dict.append(paper)
 
     import datetime
     
