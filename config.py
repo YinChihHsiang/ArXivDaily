f90aa4b40c01cd7cc6575e09da1bc5f510b98f97
YinChihHsiang <Chihhsiangyin2001@gmail.com>
2025-06-27 11:14:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,10 @@ REPO_OWNER = 'YinChihHsiang'
 REPO_NAME = 'ArXivDaily'
 
 # Set new submission url of subject
-NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
+NEW_SUB_URLS = [
+    'https://arxiv.org/list/astro-ph/new',
+    'https://arxiv.org/list/gr-qc/new'
+]
 
 # Keywords to search
 KEYWORD_LIST = ["dark energy","black hole","modified gravity","general relativity","gravitational wave"]
