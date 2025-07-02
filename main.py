5f944e9319b8adf845cc5794c9051f926e63c98b
YinChihHsiang <Chihhsiangyin2001@gmail.com>
2025-07-02 16:26:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def main(args):
         full_report = full_report + report + '\n'
         
     full_report = full_report + '\n\n'
-    full_report = full_report + 'by olozhika (Xing Yuchen). ' + '\n'
+    full_report = full_report + 'by YinChihHsiang (Yin ZhiXiang). ' + '\n'
     full_report = full_report + '\n\n'
     full_report = full_report + datetime.datetime.now().strftime("%Y-%m-%d") + '\n'
 
