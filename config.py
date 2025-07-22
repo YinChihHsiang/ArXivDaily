4df56e68623c0f457e092d77a18c111f97f20a56
YinChihHsiang <Chihhsiangyin2001@gmail.com>
2025-07-22 14:52:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -22,5 +22,5 @@ NEW_SUB_URLS = [
 # Keywords to search
 KEYWORD_LIST = ["dark energy","black hole","modified gravity","general relativity","gravitational wave","shadow","light ring"]
 # Keywords to exclude
-KEYWORD_EX_LIST = ["AGN","active galactic nucleus"]
+KEYWORD_EX_LIST = ["AGN","active galactic nucleus","stellar-mass black holes"]
 # Note that the 'Keywords' above are actually searched in the abstract instead of the real keyword section. 
